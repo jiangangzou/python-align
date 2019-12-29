360b893ad03ed137beaca58604e7ae2ecdc409f3
1083140621@qq.com <1083140621@qq.com>
2019-12-29 11:28:19 +0800

    [add]完成了外星人侵入的基本结构

---
+++ alien_invasion.py
@@ -0,0 +1,39 @@
+import pygame
+
+from settings import Settings
+
+from ship import Ship
+
+import game_functions as gf
+
+from pygame.sprite import Group
+
+def run_game():
+
+    # 初始化游戏，并创建一个屏幕对象
+    pygame.init()
+    # screen = pygame.display.set_mode((1200, 800))
+    ai_settings = Settings()
+    screen = pygame.display.set_mode(
+            (ai_settings.screen_width, ai_settings.screen_height)
+        )
+    pygame.display.set_caption("Alien Invasion")
+
+    # 设置背景颜色
+    # bg_color = (230, 230, 230)
+
+    ship = Ship(ai_settings, screen)
+
+    # 创建一个用于存储子弹的编组
+    bullets = Group()
+
+    # 开始游戏的主循环
+    while True:
+
+        # 监视键盘和鼠标事件
+        gf.check_events(ai_settings, screen, ship, bullets)
+        ship.update()
+        gf.update_bullets(bullets)
+        gf.update_screen(ai_settings, screen, ship, bullets)
+
+run_game()
